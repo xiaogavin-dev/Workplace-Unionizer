5f97e7ae8ad9c08bcb3f42c840c3e03d1ed9b3b2
Afnan Ebrahim <afnan.ebrahim214@gmail.com>
2024-09-18 19:08:04 -0400

    small change

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
 
 port = 4000
 
 app.get('/', (req, res, next) => {
     console.log("we hit this endpoint!")
-    res.send('Getting workplace info!')
+    res.status(200).json({ message: "request received" })
 })
 
 app.listen(port, () => {
