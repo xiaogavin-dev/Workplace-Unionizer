c740799063d7e0f36bd5b5af90add4176d8b6174
Afnan Ebrahim <afnan.ebrahim214@gmail.com>
2024-09-18 04:17:13 -0400

    small change

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 
-port = 3000
+port = 4000
 
 app.get('/', (req, res, next) => {
+    console.log("we hit this endpoint!")
     res.send('Getting workplace info!')
 })
 
