4323039d26da5e97e3119ee76d8f60cf751467a1
Afnan Ebrahim <afnan.ebrahim214@gmail.com>
2024-10-05 06:55:21 -0400

    make request to backend

--- src/app/(root)/search/page.tsx
+++ src/app/(root)/search/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
     Form,
     FormControl,
@@ -14,9 +14,28 @@ import { Input } from "@/components/ui/input"
 import { useForm } from 'react-hook-form'
 import { zodResolver } from "@hookform/resolvers/zod"
 import { z } from "zod"
+import getUnions from "./actions"
 import "./search.css"
-
+interface Unions {
+    id: string,
+    unionName: string
+}
 const search = () => {
+    const [allUnions, setAllUnions] = useState<Array<Unions> | undefined>([])
+    useEffect(() => {
+        const getUnionsCall = async () => {
+            try {
+                const allUnionsRes = await getUnions()
+                console.log(allUnionsRes)
+                setAllUnions(allUnionsRes)
+            }
+            catch (e: any) {
+                console.error(e.message)
+            }
+        }
+        getUnionsCall()
+
+    }, [])
     const formSchema = z.object({
         unionname: z.string(),
         location: z.string(),
@@ -33,56 +52,57 @@ const search = () => {
     })
 
     async function onSubmit(values: z.infer<typeof formSchema>) {
+
     }
 
 
     return (
         <div className='search-page-container'>
             <Form {...form}>
-            <div className='flex-center'>
-                <h1>Find A Union</h1>
-            </div>
-            <form onSubmit={form.handleSubmit(onSubmit)}>
-                <FormField
-                    control={form.control}
-                    name="unionname"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Search</FormLabel>
-                            <FormControl>
-                                <Input placeholder="Union Name" {...field} />
-                            </FormControl>
-                        </FormItem>
-                    )}
+                <div className='flex-center'>
+                    <h1>Find A Union</h1>
+                </div>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                    <FormField
+                        control={form.control}
+                        name="unionname"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Search</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Union Name" {...field} />
+                                </FormControl>
+                            </FormItem>
+                        )}
                     />
                     <FormField
-                    control={form.control}
-                    name="location"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Location</FormLabel>
-                            <FormControl>
-                                <Input placeholder="City, State" {...field} />
-                            </FormControl>
-                        </FormItem>
-                    )}
+                        control={form.control}
+                        name="location"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Location</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="City, State" {...field} />
+                                </FormControl>
+                            </FormItem>
+                        )}
                     />
                     <FormField
-                    control={form.control}
-                    name="organization"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Organization</FormLabel>
-                            <FormControl>
-                                <Input placeholder="Organization Name" {...field} />
-                            </FormControl>
-                        </FormItem>
-                    )}
+                        control={form.control}
+                        name="organization"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Organization</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Organization Name" {...field} />
+                                </FormControl>
+                            </FormItem>
+                        )}
                     />
 
-                <Button id="submit" type="submit">Submit</Button>
-            </form>
-        </Form>
+                    <Button id="submit" type="submit">Submit</Button>
+                </form>
+            </Form>
         </div>
     )
 }
