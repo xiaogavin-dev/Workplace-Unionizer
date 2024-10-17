c528aa34123480cfd544dd08eea97fe698353782
xiaogavin-dev <gavin.xiao89@myhunter.cuny.edu>
2024-10-17 17:00:40 -0400

    small cs tweaks, changed root to /search instead of /

--- src/app/(root)/search/page.tsx
+++ src/app/(root)/search/page.tsx
@@ -151,7 +151,7 @@ const Search = () => {
                             </Button>
                         </form>
 
-                        {error && <p>{error}</p>}
+                        {error && <p id="error-message">{error}</p>}
 
                         {allUnions?.length > 0 && (
                             <div className='union-results'>
