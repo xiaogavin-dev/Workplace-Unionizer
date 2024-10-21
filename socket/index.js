fedb7223516a8b6f2ddbd0e76195c6a15ac08252
Afnan Ebrahim <afnan.ebrahim214@gmail.com>
2024-10-21 12:05:37 -0400

    socket implementation

--- socket/index.js
+++ socket/index.js
@@ -5,14 +5,12 @@ const onlineUsers = [];
 const rooms = {};
 const addUser = (user, room, socketId) => {
     if (!user || !room) return { error: "User and room are required" };
-
     const exists = onlineUsers.findIndex((item) => item.uid === user.uid);
     if (exists !== -1 && onlineUsers[exists].room === room) {
         onlineUsers.splice(exists, 1);
         const roomUserIndex = rooms[room].findIndex((item) => item.uid === user.uid);
         rooms[room].splice(roomUserIndex, 1);
     }
-
     user.socketId = socketId;
     if (!rooms[room]) {
         rooms[room] = [];  // Initialize the room array properly
@@ -24,11 +22,12 @@ const addUser = (user, room, socketId) => {
 const socketInit = (server) => {
     const io = socket(server, {
         cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] }
-
     })
     io.on("connection", (socket) => {
+        console.log('A user connected:', socket.id);
         socket.on("join_room", (user, room) => {
             addUser(user, room.room, socket.id)
+            console.log("user was connected", user.displayName)
             io.in(room).emit("USER_ADDED", onlineUsers)
         });
         socket.on("SEND_MSG", (msg, room) => {
@@ -39,7 +38,6 @@ const socketInit = (server) => {
             console.log('A user disconnected:', socket.id);
         });
     });
-
 }
 
 module.exports = socketInit
