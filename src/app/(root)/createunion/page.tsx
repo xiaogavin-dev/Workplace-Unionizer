d373da4c218fa3f8ffee2cb05ad0fca6c1b77579
xiaogavin-dev <gavin.xiao89@myhunter.cuny.edu>
2024-12-01 19:43:38 -0500

    added disabled on button after union creation

--- src/app/(root)/createunion/page.tsx
+++ src/app/(root)/createunion/page.tsx
@@ -5,6 +5,7 @@ import { useAppSelector } from '@/lib/redux/hooks/redux';
 import { useRouter } from 'next/navigation';
 import { User } from 'firebase/auth';
 import "./createunion.css";
+import PropagateLoader from 'react-spinners/PropagateLoader';
 
 const CreateUnion = () => {
     const router = useRouter();
@@ -16,6 +17,8 @@ const CreateUnion = () => {
     const [visibility, setVisibility] = useState('public');
     const [message, setMessage] = useState('');
     const { user } = useAppSelector(state => state.auth) as { user: User | null };
+    const [loading, setLoading] = useState<boolean>(false);
+    const [toggle, setToggle] = useState<boolean>(false);
 
     const handleUnionNameChange = (e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value);
     const handleDescriptionChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => setDescription(e.target.value);
@@ -69,6 +72,7 @@ const CreateUnion = () => {
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         setMessage('');
+        setLoading(true);
 
         try {
             const userId = user?.uid;
@@ -102,6 +106,7 @@ const CreateUnion = () => {
 
             const data = await response.json();
             if (response.ok) {
+                setToggle(true);
                 setMessage('Union successfully added to the database!');
                 if (data.id) {
                     setTimeout(() => {
@@ -117,6 +122,8 @@ const CreateUnion = () => {
             console.error('An error occurred:', error);
             setMessage('An error occurred while submitting the form.');
         }
+
+        setLoading(false);
     };
 
     return (
@@ -267,8 +274,8 @@ const CreateUnion = () => {
                                 </label>
                             </div>
                         </div>
-
-                        <button type="submit" className="submit-btn">Submit</button>
+                        {loading ? <PropagateLoader className='align-self-center' /> : 
+                                <button type="submit" className="submit-btn" disabled={toggle}>Submit</button>}
                         {message && <p className="message">{message}</p>}
                     </form>
                 </div>
